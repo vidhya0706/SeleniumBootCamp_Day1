3977b1b5f2379b243f60ca4f5632ae59cb2eb655
govin <govin@LAPTOP-JR7522DR>
2022-09-25 00:11:40 +0530

    TestNg Report

--- src/main/java/sprint1/day1/TC003_DeleteOpportunity.java
+++ src/main/java/sprint1/day1/TC003_DeleteOpportunity.java
@@ -1,16 +1,10 @@
 package sprint1.day1;
 
-import java.time.Duration;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.Test;
-
-import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class TC003_DeleteOpportunity extends BaseClass {
 
